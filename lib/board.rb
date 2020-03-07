170727c1a4014bad68e2c1e1f9df5efcdfc976b6
Rafael Garcia <rafagrcb@gmail.com>
2020-03-07 23:22:09 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,5 @@
 # Define the variable board below.
 my_variable = "board"
-my_array = ["","",""]
-my_array = ["","",""]
-my_array = ["","",""]
+my_array = ["0","1","2"]
+my_array = ["3","4","5"]
+my_array = ["6","7","8"]
